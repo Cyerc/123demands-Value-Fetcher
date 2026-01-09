2f54eb6035a29f5a83f7b72b2e02c917d926c794
Cyerc <vexalotl@gmail.com>
2026-01-09 17:01:04 +0100

    Create Source.lua

---
+++ Source.lua
@@ -0,0 +1,26 @@
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+
+local LocalPlayer = Players.LocalPlayer
+
+local ItemList = {
+    ["Murderers vs Sheriffs"] = {},
+    ["Blox Fruits"] = {}
+}
+
+local Urls = {
+    ["Murderers vs Sheriffs"] = "https://123demands.com/api/value-list?gameId=mvsd",
+    ["Blox Fruits"] = "https://123demands.com/api/value-list?gameId=bf"
+}
+
+for GameName, Url in pairs(Urls) do
+    for Index, Value in pairs(HttpService:JSONDecode(game:HttpGet(Url))) do
+        ItemList[GameName][string.lower(Value.pets)] = Value
+    end
+end
+
+local GetItemInfo = function(Game, Name, Property)
+    return ItemList[Game][Name][Property]
+end
+
+print(GetItemInfo("Murderers vs Sheriffs", "Chrome Knife", "value"))
